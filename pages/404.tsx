e477f54236da948325c4ca7634428e876bc29c0b
Shahariar Niloy <shahariarniloyblv@gmail.com>
2022-05-10 11:42:56 +0600

    404 page

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+  return (
+    <div>
+      <h1>404 page not found no where</h1>
+    </div>
+  );
+};
+
+export default PageNotFound;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 function Home() {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("Placing an order");
+    router.push("/product");
+  };
   return (
     <div>
       <h1>Home Page</h1>
+      <Link href={"/blog"}>
+        <a>
+          Blog
+          <br />
+        </a>
+      </Link>
+      <Link href={"/product"}>
+        <a> Product</a>
+      </Link>
+      <button onClick={handleClick}>Place order</button>
     </div>
   );
 }
